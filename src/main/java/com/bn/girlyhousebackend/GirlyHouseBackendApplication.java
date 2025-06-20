c41e97911b377e63955a0717a78db7d1ad5cb8cb
bunhor-deb <bunhor0303@gmail.com>
2025-06-20 22:31:47 +0700

    init commit

---
+++ src/main/java/com/bn/girlyhousebackend/GirlyHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bn.girlyhousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlyHouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlyHouseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bn/girlyhousebackend/GirlyHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.girlyhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlyHouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
